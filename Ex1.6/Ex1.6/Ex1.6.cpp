d42fb63b0302447e3db5fc696c29ffc9bafc72f1
yevhen.nosulko <yevhen.nosulko@gmail.com>
2014-05-23 15:23:33 +0300

    Added exercise 1.6

---
+++ Ex1.6/Ex1.6/Ex1.6.cpp
@@ -0,0 +1,21 @@
+﻿/* Упражнение 1.6. Убедитесь в том, что выражение getchar() != EOF получает значение 0 или 1 . */
+
+#include "stdafx.h"
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	int b;
+
+	printf("Verifying \"getchar() != EOF\" returns \"1\" or \"0\".\nTo get \"1\" enter some character o digit and press Enter.\nTo get \"0\" press Ctrl+C.\n");
+
+	a = getchar();
+	b = a != EOF;
+
+	if (b)
+	{
+		printf("%d\t%c\n", b, a);
+	}
+	else printf("%d\n", b);
+}
