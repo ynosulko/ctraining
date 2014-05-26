fba65db9eadc6b2bb7f52ca3adbccc67fb6d8ebe
yevhen.nosulko <yevhen.nosulko@gmail.com>
2014-05-26 13:00:43 +0300

    Added exercise 1.8

---
+++ Ex1.8/Ex1.8/Ex1.8.cpp
@@ -0,0 +1,20 @@
+﻿/* Упражнение 1.8. Напишите программу для подсчета пробелов, табуляций и новых строк. */
+
+#include "stdafx.h"
+#include <stdio.h>
+
+int main()
+{
+	int a, spaces, tabs, newLines;
+	spaces = tabs = newLines = 0;
+	
+	while ((a = getchar()) != EOF) {
+		if (a == ' ')
+			++spaces;
+		else if (a == '\t')
+			++tabs;
+		else if (a == '\n')
+			++newLines;
+	}
+	printf("%d %d %d\n", spaces, tabs, newLines);
+}
