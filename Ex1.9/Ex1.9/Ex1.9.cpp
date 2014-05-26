794851482cc2d7a9186feee9e7ff3233839e9e94
yevhen.nosulko <yevhen.nosulko@gmail.com>
2014-05-26 18:10:29 +0300

    Added exercise 1.9

---
+++ Ex1.9/Ex1.9/Ex1.9.cpp
@@ -0,0 +1,22 @@
+﻿/* Упражнение 1.9. Напишите программу, копирующую символы ввода в выходной поток и заменяющую 
+стоящие подряд пробелы на один пробел. */
+
+#include "stdafx.h"
+#include <stdio.h>
+
+int main()
+{
+	int curr, prev = 0;
+
+	while ((curr = getchar()) != EOF)
+	{
+		if (curr == ' ')
+		{
+			if (prev != ' ')
+				putchar(curr);
+		}
+		else
+			putchar(curr);
+		prev = curr;
+	}
+}
