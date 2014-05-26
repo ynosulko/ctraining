56cc91ee9d6fce25f85ffe22fee2dfffc6615e07
ynosulko <yevhen.nosulko@gmail.com>
2014-05-26 23:43:11 +0300

    Added exercise 1.11

---
+++ Ex1.11/Ex1.11/Ex1.11.cpp
@@ -0,0 +1,27 @@
+﻿/* Упражнение 1.11.Как протестировать программу подсчета слов ? Какой ввод вероятнее всего обнаружит
+ошибки, если они были допущены ? */
+
+#include "stdafx.h"
+#include <stdio.h> 
+
+#define IN 1 /* внутри слова */ 
+#define OUT 0 /* вне слова */ 
+
+/* подсчет строк, слов и символов */
+int main()
+{
+	int c, nl, nw, nc, state;
+	state = OUT;
+	nl = nw = nc = 0;
+	while ((c = getchar()) != EOF) {
+		++nc;
+		if (c == '\n')
+			++nl;		if (c == ' ' || c == '\n' || c == '\t')
+			state = OUT;
+		else if (state == OUT) {
+			state = IN;
+			++nw;
+		}
+		printf("%d %d %d\n", nl, nw, nc);
+	}
+}
