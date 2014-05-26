f7cc24f7ee8ace719523ebf179d78294284f31cb
ynosulko <yevhen.nosulko@gmail.com>
2014-05-26 23:24:58 +0300

    Added exercise 1.10

---
+++ Ex1.10/Ex1.10/Ex1.10.cpp
@@ -0,0 +1,20 @@
+﻿/* Упражнение 1.10. Напишите программу, копирующую вводимые символы в выходной поток с заменой
+символа табуляции на \t, символа забоя на \b и каждой обратной наклонной черты на \\. Это сделает
+видимыми все символы табуляции и забоя. */
+
+#include "stdafx.h"
+#include <stdio.h>
+
+int main()
+{
+	int input = 0;
+
+	while ((input = getchar()) != EOF)
+	if (input == '\t')
+		printf("\\t");
+	else if (input == '\b')
+		printf("\\b");
+	else if (input == '\\')
+		printf("\\\\");
+	else putchar(input);
+}
