866d4f2bff7ef68adacf317e89295efe25890559
yevhen.nosulko <yevhen.nosulko@gmail.com>
2014-05-28 22:40:47 +0300

    Added exercise 1.13

---
+++ Ex1.13/Ex1.13/Ex1.13.cpp
@@ -0,0 +1,28 @@
+﻿/* Упражнение 1.13. Напишите программу, печатающую гистограммы длин вводимых слов. Гистограмму легко 
+рисовать горизонтальными полосами. Рисование вертикальными полосами — более трудная задача. */
+
+#include "stdafx.h"
+#include <stdio.h>
+#define IN 1 /* внутри слова */ 
+#define OUT 0 /* вне слова */
+
+
+int main()
+{
+	int input, state;
+
+	state = OUT;
+
+	while ((input = getchar()) != EOF) {
+		if (input == ' ' || input == '\n' || input == '\t') {
+			if (state == IN) {
+				putchar('\n');
+				state = OUT;
+			}
+		}
+		else {
+			state = IN;
+			putchar('-');
+		}
+	}
+}
